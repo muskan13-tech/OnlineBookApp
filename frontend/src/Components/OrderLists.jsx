c147498cc57207d5800b528f5dbfb778f53263b3
Muskan Baranwal <muskan.baranwal@accolitdigital.com>
2023-10-09 11:08:23 +0530

    frontend

---
+++ frontend/src/Components/OrderLists.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import OrderCard from "./OrderCard";
+import { Box, Heading, Stack } from "@chakra-ui/react";
+
+const OrderLists = ({ orders }) => {
+  return (
+    <Box width={"full"} p={4} height={"100%"} mt={"70px"} background={"#F9E795"} color={"black"} border={"2px solid teal"}>
+      <Heading as="h1" mb={4} textAlign={"center"}>
+        Orders List
+      </Heading>
+
+      <Stack
+        spacing={4}
+      >
+        {orders.map((order, i) => (
+          <OrderCard key={i} item={order} />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
+
+export default OrderLists;
